f97dd1022b77a4e310b6a720117ba2636a92ac75
suvakanta1808 <suvakantam11@gmail.com>
2022-09-10 20:18:20 +0530

    add models

---
+++ models/journal.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const User = require('./user');
+
+const journalSchema = new Schema(
+    {
+        dop: { 
+            type: String,
+        },
+        authorId: { 
+            type: String,
+            required: [true, "author id can't be empty"]
+        },
+        content: { 
+            type: String,
+            required: [true, "content can't be empty"]
+        },
+        likes: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        dislikes:  [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    }
+);
+
+const Journal = mongoose.model("Journal", journalSchema);
+module.exports = { Journal };
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const Journal = require('./journal');
+
+const userSchema = new Schema(
+    {
+        fullName: { 
+            type: String,
+        },
+        username: { 
+            type: String,
+            required: [true, "username can't be empty"]
+        },
+        password: {
+            type: String,
+            required: [true, "password can't be empty"]
+        },
+        journals: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Journal'
+        }]
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
