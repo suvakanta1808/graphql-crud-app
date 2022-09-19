381a89ffa8dc76919a3f79fa90ed98de2bfb6be5
suvakanta1808 <suvakantam11@gmail.com>
2022-09-19 14:31:14 +0530

    fix config

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = new ApolloServer({
   async function startServer() {
     try {
         await connectDB();
-        server.listen().then(({ url }) => {
+        server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
             console.log(`🚀 Server ready at ${url}`);
             });
     } catch (error) {
